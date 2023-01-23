577389bd6a7b2788ef87514c05b34bcba549fba5
gvart <gladis.vladlen+github@gmail.com>
2023-01-23 22:59:52 +0100

    fix: add missing library

--- src/main/java/com/everynote/perister/entrypoint/LambdaFunction.java
+++ src/main/java/com/everynote/perister/entrypoint/LambdaFunction.java
@@ -3,6 +3,8 @@ package com.everynote.perister.entrypoint;
 
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
 import com.everynote.perister.domain.ProcessNoteRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.function.Consumer;
@@ -10,6 +12,7 @@ import java.util.function.Consumer;
 @Component
 public class LambdaFunction implements Consumer<SQSEvent> {
 
+    private static final Logger log = LoggerFactory.getLogger(LambdaFunction.class);
     private final ProcessNoteRequest processNoteRequest;
 
     public LambdaFunction(ProcessNoteRequest processNoteRequest) {
@@ -18,9 +21,13 @@ public class LambdaFunction implements Consumer<SQSEvent> {
 
     @Override
     public void accept(SQSEvent sqsEvent) {
-        sqsEvent.getRecords()
-                .stream()
-                .map(SQSEvent.SQSMessage::getBody)
-                .forEach(processNoteRequest::process);
+        try {
+            sqsEvent.getRecords()
+                    .stream()
+                    .map(SQSEvent.SQSMessage::getBody)
+                    .forEach(processNoteRequest::process);
+        } catch (Exception exception) {
+            log.error("Failed to process records", exception);
+        }
     }
 }
