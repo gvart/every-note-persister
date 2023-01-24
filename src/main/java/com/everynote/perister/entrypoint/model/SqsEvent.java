ec2f7052243bb13f8865d6aa62352af149ef91ad
gvart <gladis.vladlen+github@gmail.com>
2023-01-24 23:34:43 +0100

    extract jackson config to props

---
+++ src/main/java/com/everynote/perister/entrypoint/model/SqsEvent.java
@@ -0,0 +1,8 @@
+package com.everynote.perister.entrypoint.model;
+
+import java.util.List;
+
+public record SqsEvent(
+        List<SqsMessage> records
+) {
+}
---
+++ src/main/java/com/everynote/perister/entrypoint/model/SqsMessage.java
@@ -0,0 +1,4 @@
+package com.everynote.perister.entrypoint.model;
+
+public record SqsMessage(String body) {
+}
