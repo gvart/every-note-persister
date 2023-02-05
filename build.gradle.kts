ce84e7f31a132e6205289d638036172891895f88
gvart <gladis.vladlen+github@gmail.com>
2023-02-05 20:00:44 +0100

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,14 +18,10 @@ val springCloudVersion: String  by project
 
 dependencies {
     implementation(platform("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion"))
-//    implementation("com.amazonaws:aws-lambda-java-events:3.11.0")
-//    implementation("com.amazonaws:aws-lambda-java-core:1.2.2")
-//    implementation("com.amazonaws:aws-lambda-java-serialization:1.0.1")
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
 
     implementation("org.springframework.cloud:spring-cloud-function-web")
-    implementation("org.springframework.cloud:spring-cloud-function-context")
     implementation("org.springframework.cloud:spring-cloud-function-adapter-aws")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
