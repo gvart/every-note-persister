7ef931d2ee90b03fdb13ebabf87d79e4ce713ed8
gvart <gladis.vladlen+github@gmail.com>
2023-02-05 20:03:25 +0100

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,10 +18,8 @@ val springCloudVersion: String  by project
 
 dependencies {
     implementation(platform("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion"))
-    implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.springframework.boot:spring-boot-starter-web")
 
-    implementation("org.springframework.cloud:spring-cloud-function-web")
+    implementation("org.springframework.cloud:spring-cloud-starter-function-web")
     implementation("org.springframework.cloud:spring-cloud-function-adapter-aws")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
