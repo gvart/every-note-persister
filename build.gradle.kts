02385dd56464299255ecf0fd2cc6633a84d8d84f
gvart <gladis.vladlen+github@gmail.com>
2023-02-05 18:24:05 +0100

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     id("org.springframework.boot") version "3.0.2"
     id("io.spring.dependency-management") version "1.1.0"
     id("org.graalvm.buildtools.native") version "0.9.19"
-//    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "com.everynote"
@@ -19,11 +18,15 @@ val springCloudVersion: String  by project
 
 dependencies {
     implementation(platform("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion"))
-    implementation("com.amazonaws:aws-lambda-java-serialization:1.0.1")
     implementation("com.amazonaws:aws-lambda-java-events:3.11.0")
     implementation("com.amazonaws:aws-lambda-java-core:1.2.2")
-    implementation("org.springframework.cloud:spring-cloud-starter-function-web:4.0.1")
-    implementation("org.springframework.cloud:spring-cloud-function-adapter-aws:4.0.1")
+
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    implementation("org.springframework.cloud:spring-cloud-function-web")
+    implementation("org.springframework.cloud:spring-cloud-function-context")
+    implementation("org.springframework.cloud:spring-cloud-function-adapter-aws")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
@@ -39,29 +42,3 @@ graalvmNative {
         }
     }
 }
-
-//tasks.withType<ShadowJar> {
-//    archiveClassifier.set("aws")
-//    dependencies {
-//        exclude("org.springframework.cloud:spring-cloud-function-web:4.0.1")
-//    }
-//
-//    mergeServiceFiles()
-//    append("META-INF/spring.handlers")
-//    append("META-INF/spring.schemas")
-//    append("META-INF/spring.tooling")
-//    transform(PropertiesFileTransformer::class.java) {
-//        paths = listOf("META-INF/spring.factories")
-//        mergeStrategy = "append"
-//    }
-//}
-
-//tasks.withType<Jar>() {
-//    archiveClassifier.set("default")
-//}
-//
-//tasks {
-//    generateResourcesConfigFile {
-//        dependsOn(shadowJar)
-//    }
-//}
