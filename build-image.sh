7d88f66913123e47c6291f4e09661f8f81e4495a
gvart <gladis.vladlen+github@gmail.com>
2023-02-06 01:01:37 +0100

    update workflows

--- build-image.sh
+++ build-image.sh
@@ -3,7 +3,7 @@ set -e
 
 JAVA_VERSION=java17
 GRAAL_VERSION=22.3.0
-GRADLE_VERSION=$(./gradlew --version | grep "Gradle" | awk '{print $2}')
+GRADLE_VERSION=7.6
 
 docker build --build-arg GRADLE_VERSION=$GRADLE_VERSION \
   --build-arg JAVA_VERSION=$JAVA_VERSION \
