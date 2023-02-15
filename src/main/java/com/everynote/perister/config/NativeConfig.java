72a69a6b06d659888fd77d72ba561f100428695b
gvart <gladis.vladlen+github@gmail.com>
2023-02-15 12:33:16 +0100

    fix: reflection

--- src/main/java/com/everynote/perister/config/NativeConfig.java
+++ src/main/java/com/everynote/perister/config/NativeConfig.java
@@ -3,6 +3,7 @@ package com.everynote.perister.config;
 import com.everynote.perister.domain.model.PersistNoteRequest;
 import com.everynote.perister.entrypoint.model.SqsEvent;
 import com.everynote.perister.entrypoint.model.SqsMessage;
+import com.everynote.perister.gateway.dynamodb.model.Note;
 import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,7 +12,8 @@ import org.springframework.context.annotation.Configuration;
 @RegisterReflectionForBinding({
         PersistNoteRequest.class,
         SqsMessage.class,
-        SqsEvent.class
+        SqsEvent.class,
+        Note.class
 })
 public class NativeConfig {
 }
